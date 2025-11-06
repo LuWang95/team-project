eee1f481edd117c3da057a38c45dab8bf842faff
franklinlu <wanglu5613@icloud.com>
2025-11-06 14:48:32 -0500

    refine the classes

---
+++ src/main/java/PraticalSection.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+public class PraticalSection extends Section {
+    PraticalSection(String sectionCode, ArrayList<Meeting> meetings) {
+        super(sectionCode, meetings);
+    }
+}
---
+++ src/main/java/SectionFactory.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public  class SectionFactory {
+    public Section createSection(String sectionCode, ArrayList<Meeting> meetings) {
+        String sectionType = sectionCode.substring(0, 3);
+        switch (sectionType){
+            case "LEC":
+                return new LectureSection(sectionCode, meetings);
+            case "TUT":
+                return new TutorialSection(sectionCode, meetings);
+            case "PRA":
+                return new PraticalSection(sectionCode, meetings);
+            default:
+                throw new IllegalArgumentException(
+                        "Wrong shape type: " + sectionType
+                );
+        }
+
+    };
+}
