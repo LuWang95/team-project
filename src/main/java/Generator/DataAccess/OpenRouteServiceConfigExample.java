abf2c05468e067b0dd83980ff7ed8a92b79ca82b
cesium132dot9 <sunc20060429@gmail.com>
2025-12-01 00:05:41 -0500

    Added example files for APIs

---
+++ src/main/java/Generator/DataAccess/OpenRouteServiceConfigExample.java
@@ -0,0 +1,10 @@
+package Generator.DataAccess;
+
+public final class OpenRouteServiceConfigExample {
+    public static final String API_KEY = "your_api_key";
+    public static final String BASE_URL = "https://api.openrouteservice.org/v2/directions/foot-walking?";
+    public static final String HEADER = "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8";
+
+    private OpenRouteServiceConfigExample() {
+    }
+}
